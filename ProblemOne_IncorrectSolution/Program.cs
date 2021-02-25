95f3ad762b11380d266f4799bb5d216e79fa77cc
99003614 <78959013+riya25gupta@users.noreply.github.com>
2021-02-25 15:59:13 +0530

    Update Program.cs

--- ProblemOne_IncorrectSolution/Program.cs
+++ ProblemOne_IncorrectSolution/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +10,27 @@ namespace ProblemOne_IncorrectSolution
     {
         static void Main(string[] args)
         {
-            //1. The following code find the sum of all the numbers between 0 and 10000
-            //2. The sum of the numbers should be for only numbers that are divisble by 3 and 5
-            int result = 0;
-            for (int i = 1; i <= 1000; i++)
+
+
+            try
             {
-                if (((i % 3) == 0) && ((i % 5) == 0))
+                //1. The following code find the sum of all the numbers between 0 and 10000
+                //2. The sum of the numbers should be for only numbers that are divisble by 3 and 5
+                int result = 0;
+                for (int i = 1; i <= 10000; i++)
                 {
-                    result =+i;
+                    if (((i % 3) == 0) && ((i % 5) == 0))
+                    {
+                        result = result + i;
+                    }
                 }
+                Console.WriteLine("The sum of all number divisble by 3 and 5 between 1 and 10000 is " + result);
             }
-            Console.WriteLine("The sum of all number divisble by 3 and 5 between 1 and 10000 is "+ result);
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
         }
     }
 }
