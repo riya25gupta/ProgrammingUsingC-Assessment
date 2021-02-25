3aa7899cd1df35b1a62052f2ceb5773c1843c58b
99003614 <78959013+riya25gupta@users.noreply.github.com>
2021-02-25 17:31:38 +0530

    Update GourmetOrganicShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace ProblemThree_CompleteSolution_BL
 {
-    public class GourmetOrganicShop : GourmetShop,ITaxCompute
+    public class GourmetOrganicShop : GourmetShop, ITaxCompute
     {
+        public int qty;
+        public double wght;
+        //public Dictionary<int, double> organicPriceDict = new Dictionary<int, double>();
         
+
+        public override double PayPerPiece(int quantity)
+        {
+            qty = quantity;
+            return qty;
+        }
+
+        public override double PayPerWeight(int weight)
+        {
+            wght = weight;
+            return weight;
+        }
+
+        public double TaxOnTotalBill()
+        {
+           
+                double amt = organicPriceDict.Value;
+                double cost = qty * wght * amt;
+                cost = cost + ((cost * 7.5) / 100);
+
+            return cost;
+
+
+        }
     }
 }
